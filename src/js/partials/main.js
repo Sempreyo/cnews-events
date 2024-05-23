e47c933b2a9e01590a7251d53394f50301e57d22
Zhukov <azhukov@cnews.ru>
2024-05-23 16:34:10 +0300

    init project

---
+++ src/js/partials/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const anchors = document.querySelectorAll('.anchor');
+
+	if (anchors && anchors.length > 0) {
+        anchors.forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+    }
+});
