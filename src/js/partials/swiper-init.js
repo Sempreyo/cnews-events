312cf052adc02cc94c1ddb6b9eb574bff761b5c0
Zhukov <azhukov@cnews.ru>
2024-05-27 18:13:22 +0300

    common components, header, hero block

--- src/js/partials/swiper-init.js
+++ src/js/partials/swiper-init.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-	
+	const topEventsCarousel = document.querySelectorAll(".hero__slider");
+
+	if (topEventsCarousel.length > 0) {
+		topEventsCarousel.forEach(elem => {
+			const slider = new Swiper(elem, {
+				slidesPerView: 1,
+				spaceBetween: 20,
+				loop: true,
+				pagination: {
+					el: ".hero__pagination",
+					clickable: true,
+				}
+			});
+		});
+	}
 });
