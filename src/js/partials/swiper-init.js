ad779d5ad3242febd0f99017798ad07c908dffec
Zhukov <azhukov@cnews.ru>
2024-07-31 18:08:12 +0300

    photos, program

--- src/js/partials/swiper-init.js
+++ src/js/partials/swiper-init.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const topEventsCarousel = document.querySelectorAll(".hero__slider");
 	const articlesCarousel = document.querySelectorAll(".articles__slider");
+	const galleryCarousel = document.querySelectorAll(".photos__slider");
 
 	if (topEventsCarousel.length > 0) {
 		topEventsCarousel.forEach(elem => {
@@ -32,7 +33,41 @@ document.addEventListener("DOMContentLoaded", () => {
 					992: {
 					  	slidesPerView: 3
 					}
-				  }
+				}
+			});
+		});
+	}
+
+	if (galleryCarousel.length > 0) {
+		galleryCarousel.forEach(elem => {
+			const slider = new Swiper(elem, {
+				slidesPerView: 1,
+				slidesPerGroup: 1,
+				spaceBetween: 20,
+				grid: {
+					fill: "row",
+					rows: 1
+				},
+				navigation: {
+					nextEl: ".photos__nav .swiper-button-next",
+					prevEl: ".photos__nav .swiper-button-prev"
+				},
+				breakpoints: {
+					576: {
+						slidesPerView: 2,
+						slidesPerGroup: 2,
+						grid: {
+							rows: 2
+						}
+					},
+					768: {
+						slidesPerView: 3,
+						slidesPerGroup: 3,
+						grid: {
+							rows: 3
+						}
+					}
+				}
 			});
 		});
 	}
